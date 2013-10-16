62e313c3901a8699c0f1e3ba0d84d936fec97435
Xin Yao <singerdmx@gmail.com>
2013-10-16 09:50:24 -0700

    Remove all namespaces & modules from spec files.

---
+++ spec/unit/connection/connection_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe VCloudSdk::Connection::Connection, :min, :all do
+  let(:url) { "https://10.147.0.0:8443" }
+
+  describe "#initialize" do
+    it "use settings in input arguments" do
+      site = double("site")
+      file_uploader = double("File Uploader")
+      conn = described_class.new(url, nil, nil, site, file_uploader)
+
+      VCloudSdk::Test.verify_settings conn,
+                                      :@site => site,
+                                      :@file_uploader => file_uploader
+    end
+  end
+end
