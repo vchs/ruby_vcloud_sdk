839a9724a2e1e0a09ec42d38b49d8a5fc36fa44a
Xin Yao <singerdmx@gmail.com>
2013-10-18 10:39:47 -0700

    Add integration test for find_vdc_by_name
    Change-Id: I16960d1fa3cdbb886910dc0e52020107d07bd324

--- spec/integration/client_spec.rb
+++ spec/integration/client_spec.rb
@@ -7,25 +7,36 @@ describe VCloudSdk::Client do
   let(:url) { ENV['VCLOUD_URL'] || 'https://10.146.21.135' }
   let(:username) { ENV['VCLOUD_USERNAME'] || 'dev_mgr@dev' }
   let(:password) { ENV['VCLOUD_PWD'] || 'vmware' }
+  let(:vdc_name) { ENV['VDC_NAME'] }
 
-  # TODO: we only have the initialize funtion in client lib code now,
-  # will add more as client lib code grow.
   describe "#initialize" do
     it "set up connection successfully" do
       described_class.new(url, username, password, {}, logger)
     end
 
     it "given incorrect url" do
-      expect {
+      expect do
         described_class.new(url + 'wronglink', username, password, {}, logger)
-      }.to raise_error(SocketError, /nodename nor servname provided,
-                                      or not known/)
+      end.to raise_error
     end
 
     it "given incorrect username/pwd" do
-      expect {
+      expect do
         described_class.new(url, username, 'wrongpassword', {}, logger)
-      }.to raise_error(RestClient::Unauthorized, /401 Unauthorized/)
+      end.to raise_error(RestClient::Unauthorized, /401 Unauthorized/)
+    end
+
+    describe "#find_vdc_by_name" do
+      subject { described_class.new(url, username, password, {}, logger) }
+
+      it "fail if targeted vdc does not exist" do
+        expect { subject.find_vdc_by_name("xxxx") }.to raise_error
+      end
+
+      it "find targeted vdc if it exists" do
+        vdc = subject.find_vdc_by_name(vdc_name)
+        vdc.should_not be_nil
+      end
     end
   end
 end
