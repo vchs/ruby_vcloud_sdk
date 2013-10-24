1de8aaa4128f5a63034ec8af60e5e68c85e4eb91
Xin Yao <singerdmx@gmail.com>
2013-10-24 10:36:21 -0700

    Refactor mock rest client and connection into class
    Change-Id: Ie24f9664342dc99148d36fd7da73c8a7cb55ceca

---
+++ spec/unit/mocks/rest_client.rb
@@ -0,0 +1,32 @@
+module VCloudSdk
+  module Mocks
+    class RestClient
+      attr_reader :url
+
+      def initialize(url)
+        @url = url
+      end
+
+      def [](value)
+        @resource = value
+        self
+      end
+
+      def get(headers = {})
+        VCloudSdk::Test::ResponseMapping
+          .get_mapping(:get, build_url).call(build_url, headers)
+      end
+
+      def post(payload, headers = {})
+        VCloudSdk::Test::ResponseMapping
+          .get_mapping(:post, build_url).call(build_url, payload, headers)
+      end
+
+      private
+
+      def build_url
+        @url + @resource
+      end
+    end
+  end
+end
