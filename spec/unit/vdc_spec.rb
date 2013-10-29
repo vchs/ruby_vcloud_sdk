a1ef457c2d32e4b2c4dde0e5a028fc13d1bef3b5
Xin Yao <singerdmx@gmail.com>
2013-10-29 11:13:46 -0700

    Add VDC.vapps method to VDC OM
    Change-Id: I833f327774dec9355dff18efc833fbe17068c0d7

--- spec/unit/vdc_spec.rb
+++ spec/unit/vdc_spec.rb
@@ -9,19 +9,29 @@ describe VCloudSdk::VDC do
   let(:logger) { VCloudSdk::Config.logger }
   let(:url) { VCloudSdk::Test::Response::URL }
 
-  let!(:mock_connection) do
+  let(:mock_connection) do
     VCloudSdk::Test.mock_connection(logger, url)
   end
 
   subject do
-    vdc_response = VCloudSdk::Xml::WrapperFactory.wrap_document(
-      VCloudSdk::Test::Response::VDC_RESPONSE)
     described_class.new(mock_connection, vdc_response)
   end
 
-  its(:storage_profiles) { should have_at_least(1).items }
+  describe "#storage_profiles" do
+    let(:vdc_response) do
+      VCloudSdk::Xml::WrapperFactory.wrap_document(
+        VCloudSdk::Test::Response::VDC_RESPONSE)
+    end
+
+    its(:storage_profiles) { should have_at_least(1).item }
+  end
 
   describe "#find_storage_profile_by_name" do
+    let(:vdc_response) do
+      VCloudSdk::Xml::WrapperFactory.wrap_document(
+        VCloudSdk::Test::Response::VDC_RESPONSE)
+    end
+
     it "return a storage profile given targeted name" do
       storage_profile = subject
         .find_storage_profile_by_name(VCloudSdk::Test::Response::STORAGE_PROFILE_NAME)
@@ -32,5 +42,26 @@ describe VCloudSdk::VDC do
       storage_profile = subject.find_storage_profile_by_name("xxxxxxx")
       storage_profile.should be_nil
     end
+  end
+
+  describe "#vapps" do
+    context "vdc has vapps" do
+      let(:vdc_response) do
+        VCloudSdk::Xml::WrapperFactory.wrap_document(
+          VCloudSdk::Test::Response::VDC_RESPONSE)
+      end
+
+      its(:vapps) { should have_at_least(1).item }
+    end
+
+    context "vdc has no vapps" do
+      let(:vdc_response) do
+        VCloudSdk::Xml::WrapperFactory.wrap_document(
+          VCloudSdk::Test::Response::EMPTY_VDC_RESPONSE)
+      end
+
+      its(:vapps) { should have(0).item }
+    end
+
   end
 end
