52ee2c7c91cebff1841e85c355c949aa99fb52f3
Xin Yao <singerdmx@gmail.com>
2013-10-29 09:02:48 -0700

    Add Catalog.delete_all_catalog_items method
    Change-Id: Icad7a97ba76fb37e36e52889083fb8e71c48d133

--- lib/ruby_vcloud_sdk/catalog.rb
+++ lib/ruby_vcloud_sdk/catalog.rb
@@ -3,13 +3,27 @@ module VCloudSdk
   class Catalog
     attr_reader :name
 
-    def initialize(catalog_xml_obj)
+    def initialize(connection, catalog_xml_obj)
+      @connection = connection
       @catalog_xml_obj = catalog_xml_obj
       @name = catalog_xml_obj.name
+      @logger = Config.logger
     end
 
     def id
       @catalog_xml_obj.href_id
+    end
+
+    def catalog_items
+      @connection.get("/api/admin/catalog/#{id}").catalog_items
+    end
+
+    def delete_all_catalog_items
+      catalog_items.each do |catalog_item_xml_obj|
+        catalog_item = @connection.get("/api/catalogItem/#{catalog_item_xml_obj.href_id}")
+        @logger.info "Deleting catalog item \"#{catalog_item.name}\""
+        @connection.delete(catalog_item.remove_link)
+      end
     end
 
   end
