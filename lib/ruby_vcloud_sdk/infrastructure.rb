96ed336ea4587d8329cfcb1acb475235cf4a89d4
Xin Yao <singerdmx@gmail.com>
2013-11-01 16:15:51 -0700

    Add module Infrastructure
    Change-Id: I9828049881e62e7d59defe6d6fb43b3e1f19fab9

---
+++ lib/ruby_vcloud_sdk/infrastructure.rb
@@ -0,0 +1,27 @@
+module VCloudSdk
+  # Shared functions by classes such as Client, Catalog and VDC
+  # Make sure instance variables @session and @connection are available
+  module Infrastructure
+
+    def find_vdc_by_name(name)
+      vdc_link = @session.org.vdc_link(name)
+      fail ObjectNotFoundError, "VDC #{name} not found" unless vdc_link
+      VCloudSdk::VDC.new(@session, @connection.get(vdc_link))
+    end
+
+    def catalogs
+      @session.org.catalogs.map do |catalog|
+        VCloudSdk::Catalog.new(@session, catalog)
+      end
+    end
+
+    def find_catalog_by_name(name)
+      catalogs.each do |catalog|
+        return catalog if catalog.name == name
+      end
+
+      nil
+    end
+
+  end
+end
