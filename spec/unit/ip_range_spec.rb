274478a51fd8544d607584be659df07cef7b8b8c
Xin Yao <singerdmx@gmail.com>
2013-11-09 23:23:34 -0800

    Add IpRange OM
    Change-Id: Ibe40ca2186275a06eb28836a4ca65193e01c11fc

---
+++ spec/unit/ip_range_spec.rb
@@ -0,0 +1,113 @@
+require "ruby_vcloud_sdk/ip_range"
+
+describe VCloudSdk::IpRange do
+
+  share_examples_for "VCloudSdk::IpRange" do |ip_range_string, n|
+    it "parses input string correctly" do
+      ip_range = described_class.new(ip_range_string).value
+      ip_range.should be_an_instance_of Array
+      ip_range.should have(n).item
+      ip_range.each do |i|
+        i.should be_an_instance_of Range
+        ip_range_start = i.first
+        ip_range_end = i.last
+        (ip_range_start.is_a?(NetAddr::CIDRv4) || ip_range_start.is_a?(NetAddr::CIDRv6))
+          .should be_true
+        (ip_range_end.is_a?(NetAddr::CIDRv4) || ip_range_end.is_a?(NetAddr::CIDRv6))
+          .should be_true
+        (ip_range_start > ip_range_end).should be_false
+      end
+    end
+  end
+
+  describe "#initialize" do
+    context "valid input string" do
+      context "a single IP address" do
+        it_should_behave_like "VCloudSdk::IpRange", "10.142.15.11", 1
+        it_should_behave_like "VCloudSdk::IpRange", "2001::", 1
+      end
+
+      context "input string uses '-' separator" do
+        it_should_behave_like "VCloudSdk::IpRange", "10.142.15.11 - 10.142.15.22", 1
+        it_should_behave_like "VCloudSdk::IpRange",
+                              "2001:0db8:85a3:0000:0000:8a2e:0370:7334-2001:0db8:85a3:0000:0000:8a2e:0370:7339",
+                              1
+      end
+
+      context "input string uses subnet mask" do
+        it_should_behave_like "VCloudSdk::IpRange", "10.142.15.0/24", 1
+        it_should_behave_like "VCloudSdk::IpRange", "fec0::/24", 1
+        it_should_behave_like "VCloudSdk::IpRange", "2001:0db8:85a3:0000:0000:8a2e:0370:7334/24", 1
+      end
+
+      context "input string uses comma separated IPs" do
+        it_should_behave_like "VCloudSdk::IpRange", "10.142.15.0, 10.142.15.4, 10.142.16.4", 3
+        it_should_behave_like "VCloudSdk::IpRange",
+                              "fec0::, 2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+                              2
+        it_should_behave_like "VCloudSdk::IpRange", "fec0::/24, 10.142.15.4/23", 2
+        it_should_behave_like "VCloudSdk::IpRange", "10.142.15.11-10.142.15.22, 10.142.2.4, 10.142.16.4/23", 3
+      end
+    end
+
+    context "invalid input" do
+      context "not a string" do
+        it "raises error" do
+          expect { described_class.new(["XX"]) }
+            .to raise_exception "Unable to parse a non-string object"
+        end
+      end
+
+      context "invalid string" do
+        it "raises error" do
+          expect { described_class.new("XX") }
+            .to raise_exception NetAddr::ValidationError, "Could not auto-detect IP version for 'XX'."
+        end
+      end
+
+      context "Invalid IP address" do
+        it "raises error" do
+          ip_range_string = "10.1.142.256"
+          expect { described_class.new(ip_range_string) }
+            .to raise_exception NetAddr::ValidationError
+          ip_range_string = "10.1.142.252 - 10.1.142.256"
+          expect { described_class.new(ip_range_string) }
+            .to raise_exception NetAddr::ValidationError
+        end
+      end
+
+      context "Incorrect separator" do
+        it "raises error" do
+          ip_range_string = "10.1.142.255 - "
+          expect { described_class.new(ip_range_string) }
+            .to raise_exception "Invalid input: 1 field/fields separated by '-'"
+          ip_range_string = "10.1.142.255 - 10.1.142.2 - 10.1.142.3"
+          expect { described_class.new(ip_range_string) }
+            .to raise_exception "Invalid input: 3 field/fields separated by '-'"
+          ip_range_string = "10.1.142.252/"
+          expect { described_class.new(ip_range_string) }
+            .to raise_exception ArgumentError,
+                                "CIDR address is improperly formatted. Missing netmask after '/' character."
+        end
+      end
+
+      context "start address is bigger than end address" do
+        it "raises error" do
+          ip_range_string = "10.1.142.255 - 10.1.142.1"
+          expect { described_class.new(ip_range_string) }
+            .to raise_error \
+              "IP 10.1.142.255 is bigger than IP 10.1.142.1"
+        end
+      end
+
+      context "bad subnet mask" do
+        it "raises error" do
+          ip_range_string = "10.1.142.0/33"
+          expect { described_class.new(ip_range_string) }
+            .to raise_error NetAddr::ValidationError,
+                            "Netmask, 33, is out of bounds for IPv4."
+        end
+      end
+    end
+  end
+end
